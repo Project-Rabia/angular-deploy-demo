ce19dbdcb2f874c36fd2a743a7f01af228eefebb
Jonathan Garbee <jonathan@garbee.me>
2022-12-22 13:26:47 -0500

    just testing

--- bin/pr-assignments.js
+++ bin/pr-assignments.js
@@ -1,4 +1,17 @@
 module.exports = async ({ github, context }) => {
-  console.log('github', github);
-  console.log('context', context);
+  const owner = context.repo.owner;
+  const repo = context.repo.repo;
+  const pull_number = context.payload.number;
+  await github.rest.issues.addAssignees({
+    owner,
+    repo,
+    issue_number: pull_number,
+    assignees: [context.payload.sender.login],
+  });
+  // await github.rest.pulls.requestReviewers({
+  //   owner,
+  //   repo,
+  //   pull_number,
+  //   reviewers: ['garbee']
+  // });
 };
