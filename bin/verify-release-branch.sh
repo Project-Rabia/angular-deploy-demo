a015e6c96da9b7f84ffedf55892b8b4d6ce5e55b
Jonathan Garbee <jonathan@garbee.me>
2022-12-23 13:54:10 -0500

    ci: verify release branches on pr

--- bin/verify-release-branch.sh
+++ bin/verify-release-branch.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
-version=$(grep -o '^Release:\s\d$' "$DESCRIPTION" | grep -oE '[0-9]+')
+releaseString=$(echo "$BODY" | grep -oE '^Release:+[[:space:]]+[0-9]+$')
+version=$(echo "$releaseString" | cut -d ":" -f 2 | xargs)
+
+# The script may be executed if the string `Release:` is used
+# within a body message. The grep checks only if it looks like
+# it should be in place of a trailer. So if the version is
+# not found, exit without error.
+if [ -z "$version" ]; then
+  exit 0
+fi
 
 git ls-remote --exit-code --heads origin "release/$version"
 
@@ -14,8 +23,19 @@ if [ "$status" -eq 1 ]; then
   {
     echo "# Generic Failure"
     echo ""
+    echo ""
+    echo "<details>"
+    echo "<summary>PR Body</summary>"
+    echo ""
+    echo "\`\`\`"
+    echo "$BODY"
+    echo "\`\`\`"
+    echo ""
+    echo "</details>"
+    echo ""
+    echo "Requested version: $version"
+    echo ""
     echo "Something went wrong checking for a remote branch"
-    echo "The value being looked for was \`release/$version\`"
   } > "$GITHUB_STEP_SUMMARY"
   exit 1
 fi
@@ -24,10 +44,20 @@ if [ "$status" -eq 2 ]; then
   {
     echo "# Does not exist"
     echo ""
-    echo "The version $version requested does not have an"
-    echo "active release branch."
-    echo "Please target an existing version or remove the"
-    echo "request for a release merge."
+    echo ""
+    echo "<details>"
+    echo "<summary>PR Body</summary>"
+    echo ""
+    echo "\`\`\`"
+    echo "$BODY"
+    echo "\`\`\`"
+    echo ""
+    echo "</details>"
+    echo ""
+    echo "Requested version: $version"
+    echo ""
+    echo "The version requested does not have an active release branch."
+    echo "Please target an existing version or remove the request for a release merge."
   } > "$GITHUB_STEP_SUMMARY"
   exit 2
 fi
